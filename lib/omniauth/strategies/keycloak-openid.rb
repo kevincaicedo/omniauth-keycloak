276b46f17a43a3f7cb96e296e68ebfc47afd9e03
Kevin Caicedo <kevin.caicedo@rockwellautomation.com>
2021-03-30 19:22:35 -0500

    change access token

--- lib/omniauth/strategies/keycloak-openid.rb
+++ lib/omniauth/strategies/keycloak-openid.rb
@@ -90,8 +90,8 @@ module OmniAuth
             end
 
             def other_phase
-                log :debug, "Test TK: #{self.access_token.token}"
-                redirect "http://localhost:3333/oauth/recived?token=#{self.access_token.token}"
+                log :debug, "Test TK: #{@access_token.token}"
+                redirect "http://localhost:3333/oauth/recived?token=#{@access_token.token}"
             end
 
             uid{ raw_info['sub'] }
