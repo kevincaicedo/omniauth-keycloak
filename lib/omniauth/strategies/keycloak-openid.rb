aceb48643078576d89ed3dcccd6a8ec4d0b2f231
Kevin Caicedo <kevin.caicedo@rockwellautomation.com>
2021-03-31 09:39:13 -0500

    disable callback method

--- lib/omniauth/strategies/keycloak-openid.rb
+++ lib/omniauth/strategies/keycloak-openid.rb
@@ -80,25 +80,25 @@ module OmniAuth
               log :debug, "Token url: #{@token_url}"
             end
 
-            def callback_phase
-                error = request.params["error_reason"] || request.params["error"]
-                if error
-                  fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
-                elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
-                  fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
-                else
-                  self.access_token = build_access_token
-                  self.access_token = access_token.refresh! if access_token.expired?
-                  #   redirect "http://localhost:3333/auth/gitlab?token=#{access_token.token}"
-                  super
-                end
-            rescue ::OAuth2::Error, CallbackError => e
-                fail!(:invalid_credentials, e)
-            rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
-                fail!(:timeout, e)
-            rescue ::SocketError => e
-                fail!(:failed_to_connect, e)
-            end
+            # def callback_phase
+            #     error = request.params["error_reason"] || request.params["error"]
+            #     if error
+            #       fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+            #     elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
+            #       fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+            #     else
+            #       self.access_token = build_access_token
+            #       self.access_token = access_token.refresh! if access_token.expired?
+            #       #   redirect "http://localhost:3333/auth/gitlab?token=#{access_token.token}"
+            #       super
+            #     end
+            # rescue ::OAuth2::Error, CallbackError => e
+            #     fail!(:invalid_credentials, e)
+            # rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
+            #     fail!(:timeout, e)
+            # rescue ::SocketError => e
+            #     fail!(:failed_to_connect, e)
+            # end
 
             def build_access_token
                 verifier = request.params["code"]
